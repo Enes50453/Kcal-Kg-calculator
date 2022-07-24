e53f4e262c765c8332cc5cca4512bd476eb159df
Enes50453 <109443767+Enes50453@users.noreply.github.com>
2022-07-24 15:30:53 +0300

    Kcal/Kg calculator
    A human should burn approximately 7500 calories for 1kg of fat loss. With this program you can save your daily burned calories and see how much fat did you burn in total time.

---
+++ Kcal/Kcal/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace Kcal
+{
+    partial class Kcal
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.progressBar1.Location = new System.Drawing.Point(249, 38);
+            this.progressBar1.Maximum = 7500;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(604, 95);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(249, 152);
+            this.progressBar2.Maximum = 7500;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(605, 95);
+            this.progressBar2.Step = 1;
+            this.progressBar2.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar2.TabIndex = 2;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(249, 266);
+            this.progressBar3.Maximum = 7500;
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(604, 95);
+            this.progressBar3.Step = 1;
+            this.progressBar3.TabIndex = 3;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(252, 380);
+            this.progressBar4.Maximum = 7500;
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(604, 95);
+            this.progressBar4.Step = 1;
+            this.progressBar4.TabIndex = 4;
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.Location = new System.Drawing.Point(252, 494);
+            this.progressBar5.Maximum = 7500;
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(605, 95);
+            this.progressBar5.Step = 1;
+            this.progressBar5.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(130, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(112, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(86, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "UPDATE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 136);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(230, 433);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(246, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "7500 /";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(287, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(246, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "7500 /";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(287, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "0";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(249, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "7500 /";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(290, 250);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(252, 364);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "7500 /";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(293, 364);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(252, 478);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(39, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "7500 /";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(293, 478);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(13, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(13, 105);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(72, 13);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "TOTAL KCAL";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(16, 39);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(108, 20);
+            this.dateTimePicker1.TabIndex = 21;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(180, 65);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(63, 23);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "DEL";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 23;
+            // 
+            // Kcal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 597);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Kcal";
+            this.Text = "Kcal";
+            this.Load += new System.EventHandler(this.Kcal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Kcal/Kcal/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Kcal
+{
+    public partial class Kcal : Form
+    {
+        OleDbConnection myconnection = new OleDbConnection("Provider=Microsoft.ACE.OleDb.12.0;Data Source=" + Application.StartupPath + "\\Kcal.accdb");
+        public Kcal()
+        {
+            InitializeComponent();
+        }
+        int newcal = 0;
+        int sumKcal = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            showlist();
+            myconnection.Open();
+            OleDbCommand datecheck = new OleDbCommand("select Kcal from Kcall where tarih=@tarih", myconnection);
+            datecheck.Parameters.Add("tarih", OleDbType.DBDate).Value = dateTimePicker1.Text;
+            OleDbDataReader readd = datecheck.ExecuteReader();
+            if (readd.Read() == true)
+            {
+                newcal = Convert.ToInt32(textBox2.Text) + Convert.ToInt32(readd["Kcal"].ToString());
+                try
+                {
+                    OleDbCommand Addline = new OleDbCommand("UPDATE Kcall set Kcal='" + newcal + "' where tarih=@tarih", myconnection);
+                    Addline.Parameters.Add("tarih", OleDbType.DBDate).Value = dateTimePicker1.Text; //***
+                     Addline.ExecuteNonQuery();
+                    myconnection.Close();
+                    showlist();
+                    calcsum();
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message, "ERROR");
+                    myconnection.Close();
+                    throw;
+                }
+            }
+            else
+            {
+                try
+                {
+                    OleDbCommand Addline = new OleDbCommand("insert into Kcall(tarih,Kcal) values('" + dateTimePicker1.Text + "','" + textBox2.Text + "')", myconnection);
+                    Addline.ExecuteNonQuery();
+                    myconnection.Close();
+                    showlist();
+                    calcsum();
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message, "ERROR");
+                    myconnection.Close();
+                    throw;
+                }
+            }
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            showlist();
+            try
+            {
+                myconnection.Open();
+                OleDbCommand updateline = new OleDbCommand("UPDATE Kcall set Kcal='" + textBox2.Text + "' where tarih=@tarih",myconnection);
+                updateline.Parameters.Add("tarih", OleDbType.DBDate).Value = dateTimePicker1.Text; //***
+                updateline.ExecuteNonQuery();
+                myconnection.Close();
+                showlist();
+                calcsum();
+            }
+            catch (Exception error)
+            {
+
+                MessageBox.Show(error.Message, "ERROR");
+                myconnection.Close();
+                throw;
+            }
+        }
+
+        private void showlist()
+        {
+            try
+            {
+                myconnection.Open();
+                OleDbDataAdapter dgvlist = new OleDbDataAdapter("select * from Kcall", myconnection);
+                DataSet dgvmemory = new DataSet();
+                dgvlist.Fill(dgvmemory);
+                dataGridView1.DataSource = dgvmemory.Tables[0];
+                myconnection.Close();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "ERROR");
+                myconnection.Close();
+                throw;
+            }
+        }
+        private void calcsum()
+        {
+            sumKcal = 0;
+            myconnection.Open();
+            OleDbCommand calcsum = new OleDbCommand("select Kcal from Kcall", myconnection);
+            OleDbDataReader readdd = calcsum.ExecuteReader();
+            while(readdd.Read())
+            {
+                sumKcal = sumKcal + Convert.ToInt32(readdd["Kcal"].ToString());
+                
+            }
+            myconnection.Close();
+            textBox1.Text = sumKcal.ToString();
+            prgsbar();
+
+        }
+        private void prgsbar()
+        {
+            if (sumKcal <= 7500)
+            {
+                progressBar1.Value = sumKcal;
+                progressBar2.Value = 0;
+                progressBar3.Value = 0;
+                progressBar4.Value = 0;
+                progressBar5.Value = 0;
+                label2.Text = progressBar1.Value.ToString();
+                label4.Text = "0";
+                label6.Text = "0";
+                label8.Text = "0";
+                label10.Text = "0";
+            }
+            else if (sumKcal > 7500 && sumKcal <= 15000)
+            {
+                progressBar1.Value = 7500;
+                progressBar2.Value = sumKcal - 7500;
+                progressBar3.Value = 0;
+                progressBar4.Value = 0;
+                progressBar5.Value = 0;
+                label2.Text = "7500";
+                label4.Text = progressBar2.Value.ToString();
+                label6.Text = "0";
+                label8.Text = "0";
+                label10.Text = "0";
+            }
+            else if (sumKcal >15000 && sumKcal <= 22500)
+            {
+                progressBar1.Value = 7500;
+                progressBar2.Value = 7500;
+                progressBar3.Value = sumKcal - 15000;
+                progressBar4.Value = 0;
+                progressBar5.Value = 0;
+                label2.Text = "7500";
+                label4.Text = "7500";
+                label6.Text = progressBar3.Value.ToString();
+                label8.Text = "0";
+                label10.Text = "0";
+            }
+            else if (sumKcal > 22500 && sumKcal <= 30000)
+            {
+                progressBar1.Value = 7500;
+                progressBar2.Value = 7500;
+                progressBar3.Value = 7500;
+                progressBar4.Value = sumKcal - 22500;
+                progressBar5.Value = 0;
+                label2.Text = "7500";
+                label4.Text = "7500";
+                label6.Text = "7500";
+                label8.Text = progressBar3.Value.ToString();
+                label10.Text = "0";
+
+            }
+            else if (sumKcal > 30000 && sumKcal <= 37500)
+            {
+                progressBar1.Value = 7500;
+                progressBar2.Value = 7500;
+                progressBar3.Value = 7500;
+                progressBar4.Value = 7500;
+                progressBar5.Value = sumKcal - 30000;
+                label2.Text = "7500";
+                label4.Text = "7500";
+                label6.Text = "7500";
+                label8.Text = "7500";
+                label10.Text = progressBar3.Value.ToString();
+            }
+            else if(sumKcal > 37500)
+            {
+                MessageBox.Show("Value is too high!");
+            }
+        }
+
+        private void Kcal_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = "0";
+            calcsum();
+            showlist();
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            showlist();
+            try
+            {
+                myconnection.Open();
+                OleDbCommand deleteline = new OleDbCommand("delete from Kcall where tarih=@tarih", myconnection);
+                deleteline.Parameters.Add("tarih", OleDbType.DBDate).Value = dateTimePicker1.Text;
+                deleteline.ExecuteNonQuery();
+                myconnection.Close();
+                showlist();
+                calcsum();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "ERROR");
+                myconnection.Close();
+                throw;
+            }
+        }
+    }
+}
